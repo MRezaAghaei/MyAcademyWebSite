1425e39f8b55db3e880e6026107f73d936d0a448
MohammadReza <MohammadReza@DESKTOP-OQC10OG>
2019-10-18 14:09:00 +0330

    Add first commit

--- Academy.DataAccessLayer/IRepository/IGenericRepository.cs
+++ Academy.DataAccessLayer/IRepository/IGenericRepository.cs
@@ -4,6 +4,7 @@ namespace Academy.DataAccessLayer.IRepository
 {
     public interface IGenericRepository<TInPut, TOutPut> where TInPut : class where TOutPut : class
     {
+        //this is git changes
         Task<TOutPut> ListAsync<Input,OutPut>(TInPut input);
     }
 }
