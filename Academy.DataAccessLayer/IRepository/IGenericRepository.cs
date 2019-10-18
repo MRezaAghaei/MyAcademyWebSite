5d2c33e20fcdf279daa7b49a471275b79e156e20
MohammadReza <MohammadReza@DESKTOP-OQC10OG>
2019-10-18 14:52:39 +0330

    test

--- Academy.DataAccessLayer/IRepository/IGenericRepository.cs
+++ Academy.DataAccessLayer/IRepository/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace Academy.DataAccessLayer.IRepository
     public interface IGenericRepository<TInPut, TOutPut> where TInPut : class where TOutPut : class
     {
 
-        //test
+        //testfffff
         Task<TOutPut> ListAsync<Input,OutPut>(TInPut input);
     }
 }
