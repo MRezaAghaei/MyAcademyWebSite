7e24a82215966f7691cf3a8f849de1828b00445f
MohammadReza <MohammadReza@DESKTOP-OQC10OG>
2019-10-18 14:25:33 +0330

    kkk

--- Academy.DataAccessLayer/IRepository/IGenericRepository.cs
+++ Academy.DataAccessLayer/IRepository/IGenericRepository.cs
@@ -4,7 +4,6 @@ namespace Academy.DataAccessLayer.IRepository
 {
     public interface IGenericRepository<TInPut, TOutPut> where TInPut : class where TOutPut : class
     {
-        //this is git changes
         Task<TOutPut> ListAsync<Input,OutPut>(TInPut input);
     }
 }
